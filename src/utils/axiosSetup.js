40c8fe01125106e896101c5b5d9c4f54bcbcfcc5
Robinsonsarangthem <robinsonsarangthem33@gmail.com>
2024-10-11 16:30:30 +0530

    utlis setup

--- src/utils/axiosSetup.js
+++ src/utils/axiosSetup.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // export const baseURL = 'https://9sjnwt2k-1000.inc1.devtunnels.ms/'
-export const baseURL = 'http://139.59.24.128:3002/api/	'
+export const baseURL = 'http://139.59.24.128:3002/	'
 
 export const Axios = axios.create({
 	baseURL,
