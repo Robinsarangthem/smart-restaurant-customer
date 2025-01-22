51c00a32fd612b35e84003f4766da4bdef06bd79
nganaremba-rem <ngancloverr+remkugithub@gmail.com>
2025-01-22 21:14:56 +0530

    latest

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,43 @@
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import fs from 'fs';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [
-		react(),
-		nodePolyfills({
-			protocolImports: true,
-		}),
-	],
+  plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true,
+    }),
+  ],
 
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	server: {
-		https: {
-			key: './smart-restaurant-privateKey.key',
-			cert: './smart-restaurant.crt',
-		},
-	},
-	build: {
-		minify: 'terser', // Ensures Terser is used for minification
-		terserOptions: {
-			compress: {
-				drop_console: true, // Removes console logs from production build
-				drop_debugger: true, // Removes debugger statements
-				unused: true,
-			},
-			mangle: true,
-			output: {
-				comments: false,
-			},
-		},
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync('./certificates/localhost-key.pem'),
+      cert: fs.readFileSync('./certificates/localhost.pem'),
+    },
+  },
+  build: {
+    minify: 'terser', // Ensures Terser is used for minification
+    terserOptions: {
+      compress: {
+        drop_console: true, // Removes console logs from production build
+        drop_debugger: true, // Removes debugger statements
+        unused: true,
+      },
+      mangle: true,
+      output: {
+        comments: false,
+      },
+    },
 
-		sourcemap: false,
-		target: 'esnext',
-	},
-})
+    sourcemap: false,
+    target: 'esnext',
+  },
+});
